513c91d7b30eba25fa863dfc42ad83cf8ca0bda0
manasa <manasa.mudham@gmail.com>
2023-03-21 06:14:29 +0000

    first commit

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,33 @@
+// Write your code here
+
+import {Component} from 'react'
+import './index.css'
+
+class Subscribe extends Component {
+  state = {isClicked: false}
+
+  isSubscribe = () => {
+    this.setState(prevValue => ({isClicked: !prevValue.isClicked}))
+  }
+
+  getButtonText = () => {
+    const {isClicked} = this.state
+    return isClicked ? 'Subscribed' : 'Subscribe'
+  }
+
+  render() {
+    const buttonTxt = this.getButtonText()
+
+    return (
+      <div className="main-container">
+        <h1 className="heading">Welcome</h1>
+        <p className="description">Thank you! Happy Learning</p>
+        <button type="button" className="s-button" onClick={this.isSubscribe}>
+          {buttonTxt}
+        </button>
+      </div>
+    )
+  }
+}
+
+export default Subscribe
